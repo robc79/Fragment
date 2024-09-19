4153db0b088ae56669def718adb36b850ac3125c
Rob Cook <message@robcook.me.uk>
2024-09-19 15:33:10 +0100

    Added web ui project.

---
+++ Infrastructure.Sql/FragmentDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Fragment.Domain.Repositories;
+using Fragment.Domain;
+
+namespace Fragment.Infrastructure.Sql;
+
+public class FragmentDbContext : DbContext, IUnitOfWork
+{
+    public FragmentDbContext(DbContextOptions<FragmentDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<TextFragment> TextFragments { get; set; }
+
+    public async Task CommitChangesAsync(CancellationToken ct)
+    {
+        _ = await SaveChangesAsync(ct);
+    }
+}
