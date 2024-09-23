d973762182570aa38be7ce5d752046fe885504de
Rob Cook <message@robcook.me.uk>
2024-09-23 14:49:29 +0100

    WIP - rest of UI for fragments.

---
+++ WebUI/Pages/Fragments/List.cshtml.cs
@@ -0,0 +1,28 @@
+using Fragment.Application.Dtos;
+using Fragment.Application.ListFragments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fragment.WebUI.Pages.Fragments;
+
+public class ListModel : PageModel
+{
+    private readonly IMediator _mediator;
+
+    public List<TextFragmentDto> Fragments { get; set; }
+
+    public ListModel(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    public async Task<IActionResult> OnGetAsync([FromQuery] int skip, [FromQuery] int take)
+    {
+        var request = new ListFragmentsRequest(skip, take);
+        var response = await _mediator.Send(request);
+        Fragments = response;
+
+        return Page();
+    }
+}
